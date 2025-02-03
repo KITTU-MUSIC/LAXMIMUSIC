e03762eae6256582281053d3e8cf58f80a01a1e6
KITTU-MUSIC <cyberyt80@gmail.com>
2025-02-03 16:17:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ANIL-BABY/LAXMIMUSIC",
+    "https://github.com/KITTU-MUSIC/LAXMIMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
